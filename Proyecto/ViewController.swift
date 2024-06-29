a9560e879eabffa585a62115bf2c1410c5f83008
rodloah <rodrigo.lovon@tecsup.edu.pe>
2024-06-29 18:07:10 -0500

    Initial Commit

---
+++ Proyecto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto
+//
+//  Created by steve on 29/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
